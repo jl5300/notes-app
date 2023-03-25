f3a55292b83882ff632115b6227891f4a3dd4a85
Joel Lozano <lozano.joel.a@gmail.com>
2023-03-24 22:54:37 -0400

    Added sessions

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import dbConfig from '../config/database.config';
 import postsRouter from './routes/post.routes';
+import MongoStore from 'connect-mongo';
+import sessions from 'express-session';
 import mongoose from 'mongoose';
+import passport from 'passport';
 
 mongoose.set('strictQuery', false);
 
@@ -16,11 +19,24 @@ app.use(express.json());
 		await mongoose.connect(dbConfig.url);
         console.log('Connected to database successfully.')
 	} catch (err: any) {
-		console.error(err.message || "Error connecting to database.");
+		console.error(err.message || 'Error connecting to database.');
 		process.exit();
 	}
 })();
 
+app.use(sessions({
+    store: MongoStore.create({
+        mongoUrl: dbConfig.url,
+        ttl: 14 * 24 * 60 * 60,
+        autoRemove: 'native'
+    }),
+    secret: '8t7ablgdg6',
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    saveUninitialized: true,
+    resave: false
+}));
+
+app.use(passport.session())
 app.use('/posts', postsRouter);
 
 app.listen(PORT, () => {
